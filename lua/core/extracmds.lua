0018a71e1b0ce6717def9a0639ecafc164986151
gleauth <authmeta@gmail.com>
2023-04-06 00:16:41 +0800

    optimize

--- lua/core/extracmds.lua
+++ lua/core/extracmds.lua
@@ -1,11 +1,5 @@
-local kopts = {noremap = true, silent = true}
-
-local function mapcmd(key, cmd)
-    vim.api.nvim_set_keymap('n', key, ':'..cmd..'<cr>', kopts)
-end
-
 -- 快速打开Lazy界面
-mapcmd("<leader>la","Lazy")
+vim.keymap.set({"n","v"},"<leader>la","<cmd>Lazy<CR>",{desc = "Open Lazy Menu"})
 -- 查看lazygit日志
 vim.keymap.set("n","<leader>ll",function()
     require("lazy.util").float_term({ "lazygit","log" })
