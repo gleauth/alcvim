9c59e2d069ff3d1b38c1309fdaad4601a2010928
gleauth <authmeta@gmail.com>
2023-04-08 18:36:15 +0800

    optimize

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,44 +1,32 @@
-local kopts = {noremap = true, silent = true}
+------ quick commands ------
+vim.keymap.set("n","<leader>ce", "<cmd>enew<CR>",{desc = "New File"})
+vim.keymap.set("n","<leader>cw", "<cmd>w<CR>",{desc = "Buffer Write File"})
+vim.keymap.set("n",'<leader>cW', "<cmd>wa<CR>",{desc = "Write All File"})
+vim.keymap.set("n",'<leader>cE', "<cmd>e!<CR>",{desc = "Revoke all file changes"})
+vim.keymap.set("n",'<leader>cq', "<cmd>q<CR>",{desc = "Quit"})
+vim.keymap.set("n",'<leader>cQ', "<cmd>q!<CR>",{desc = "Force Quit"})
+vim.keymap.set("n",'<leader>cx', "<cmd>x<CR>",{desc = "Save And Quit"})
+vim.keymap.set("n","<leader>ch", "<cmd>checkhealth<CR>",{desc = "Neovim Health Check"})
 
-local function mapkey(mode, lhs, rhs)
-    vim.api.nvim_set_keymap(mode, lhs, rhs, kopts)
-end
-
-local function mapcmd(key, cmd)
-    vim.api.nvim_set_keymap('n', key, ':'..cmd..'<cr>', kopts)
-end
-
-mapkey('n', '<leader>ce', ':e<space>')
-mapcmd('<leader>cw', 'w')
-mapcmd('<leader>cW', 'wa')
-mapcmd('<leader>cE', 'e!')
-mapcmd('<leader>cq', 'q')
-mapcmd('<leader>cQ', 'q!')
-mapcmd('<leader>cx', 'x')
-
--- 健康检查
-mapcmd("<leader>ch","checkhealth")
--- search
-mapkey('n', '<leader>se', '/\\<lt>\\><left><left>')
-
+------ keybinds ------
 -- window
-mapkey('n', '<leader>wh', '<c-w>h')
-mapkey('n', '<leader>wj', '<c-w>j')
-mapkey('n', '<leader>wk', '<c-w>k')
-mapkey('n', '<leader>wl', '<c-w>l')
-mapkey('n', '<leader>w1', '<c-w>o')
-mapkey("n", "<leader>wv", "<c-w>v") -- 水平新增窗口
-mapkey("n", "<leader>ws", "<c-w>s") -- 垂直新增窗口
-mapcmd('<leader>wx', 'x')
-mapcmd('<leader>w2', 'sp')
-mapcmd('<leader>w3', 'vs')
--- window resize
-mapkey('n', '<M-9>', '<c-w><')
-mapkey('n', '<M-0>', '<c-w>>')
-mapkey('n', '<M-->', '<c-w>-')
-mapkey('n', '<M-=>', '<c-w>+')
-mapkey('n', '<M-r>', 'resize<space>')
-mapkey('n', '<M-t>', 'vertical resize<space>')
---单行或多行移动
-mapkey("v", "J", ":m '>+1<CR>gv=gv")
-mapkey("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('n', '<leader>wh', '<c-w>h',{desc = "Goto left window"})
+vim.keymap.set('n', '<leader>wj', '<c-w>j',{desc = "Goto lower window"})
+vim.keymap.set('n', '<leader>wk', '<c-w>k',{desc = "Goto upper window"})
+vim.keymap.set('n', '<leader>wl', '<c-w>l',{desc = "Goto right window"})
+vim.keymap.set("n", "<leader>wv", "<c-w>v",{desc = "Horizontal addition window"})
+vim.keymap.set("n", "<leader>ws", "<c-w>s",{desc = "Vertical addition window"})
+vim.keymap.set('n', '<M-9>', '<c-w><',{desc = "Reduce window width"})
+vim.keymap.set('n', '<M-0>', '<c-w>>',{desc = "Increase window width"})
+vim.keymap.set('n', '<M-->', '<c-w>-',{desc = "Reduce window height"})
+vim.keymap.set('n', '<M-=>', '<c-w>+',{desc = "Increase window height"})
+-- line move
+vim.keymap.set("v","J",":m '>+1<CR>gv=gv")
+vim.keymap.set("v","K",":m '<-2<CR>gv=gv")
+vim.keymap.set("n","j","v:count == 0 ? 'gj' : 'j'",{expr = true,silent = true})
+vim.keymap.set("n","k","v:count == 0 ? 'gk' : 'k'",{expr = true,silent = true})
+-- indent
+vim.keymap.set("v","<","<gv")
+vim.keymap.set("v",">",">gv")
+-- search
+vim.keymap.set('n', '<leader>se', '/\\<lt>\\><left><left>')
