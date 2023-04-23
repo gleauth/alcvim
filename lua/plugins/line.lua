b271558ae7322fd6641120415cface0146a091a1
gleauth <authmeta@gmail.com>
2023-04-24 02:35:43 +0800

    optimize

--- lua/plugins/line.lua
+++ lua/plugins/line.lua
@@ -94,11 +94,6 @@ return {
                     lualine_c = {},
                     lualine_x = {
                         {
-                            require("lazy.status").updates,
-                            cond = require("lazy.status").has_updates,
-                            color = { fg = "#ff9e64" },
-                        },
-                        {
                             require("noice").api.status.message.get_hl,
                             cond = require("noice").api.status.message.has,
                         },
@@ -194,6 +189,11 @@ return {
                 },
             }
             ins_left {
+                require("lazy.status").updates,
+                cond = require("lazy.status").has_updates,
+                color = { fg = "#ff9e64" },
+            }
+            ins_left {
                 function()
                     return '%='
                 end,
@@ -204,13 +204,13 @@ return {
                     local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
                     local clients = vim.lsp.get_active_clients()
                     if next(clients) == nil then
-                    return msg
+                        return msg
                     end
                     for _, client in ipairs(clients) do
-                    local filetypes = client.config.filetypes
-                    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                        return client.name
-                    end
+                        local filetypes = client.config.filetypes
+                        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                            return client.name
+                        end
                     end
                     return msg
                 end,
