800807997af83a28bb7c9b76e8e9a70c6e88435e
gleauth <authmeta@gmail.com>
2023-05-08 02:07:13 +0800

    remove nvim-spider config

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -121,7 +121,7 @@ return {
             {"b",mode = {"n","o","x"}},
             {"ge",mode = {"n","o","x"}}
         },
-        init = function ()
+        config = function ()
             vim.keymap.set({"n","o","x"},"w","<cmd>lua require('spider').motion('w')<CR>",{desc = "Spider-w"})
             vim.keymap.set({"n","o","x"},"e","<cmd>lua require('spider').motion('e')<CR>",{desc = "Spider-e"})
             vim.keymap.set({"n","o","x"},"b","<cmd>lua require('spider').motion('b')<CR>",{desc = "Spider-b"})
